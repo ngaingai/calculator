016e2ba00226ce4765760ba6a85f57e00f948bfe
Alex N <alex.ngai@gmail.com>
2020-05-12 14:17:39 +0900

    Operator logic working

--- calculator.js
+++ calculator.js
@@ -1,3 +1,13 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('#display');
+
+let num1 = 0;
+let num2 = 0;
+let displayValue = '';
+let gotNum1 = false;
+let operator = '';
+let result = 0;
+
 function add (a, b) {
     return a + b;
 }
@@ -15,50 +25,48 @@ function divide (a, b) {
 }
 
 function operate (num1, operator, num2) {
+    console.log(num1+operator+num2+"in operate");
     if (operator === '+') {
-        console.log(add(num1, num2));
+        result = add(+num1, +num2); // Converts string to number
     } else if (operator === '-') {
-        console.log(subtract(num1, num2));
+        result = subtract(num1, num2);
     } else if (operator === '*') {
-        console.log(multiply(num1, num2));
+        result = multiply(num1, num2);
     } else if (operator === '/') {
-        console.log(divide(num1, num2));
+        result = divide(num1, num2);
     }
+    return result;
 }
 
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', (e) => {    // Set event listeners to all buttons
+        if (buttons[i].className === "number" ) {
+            if (gotNum1 === false) {
+                display.textContent += e.target.id; // Input numbers until operator
+                displayValue = display.textContent;
+                num1 = displayValue;                // Assign num1
+            } else if (gotNum1 === true) {
+                display.textContent += e.target.id;
+                displayValue = display.textContent;
+                num2 = displayValue;
+            }
+        }
+        else if (buttons[i].className === "operator" && gotNum1 === false) {
+            operator = e.target.id;    // Assign operator
+            display.textContent = '';               // Clear screen
+            gotNum1 = true;             // Start looking for num2
+        }
+        else if (buttons[i].className === "equals") {
+            result = operate(num1, operator, num2);
+            display.textContent = result;
+        }
+        else if (buttons[i].className === "clear") {
+            display.textContent = '';
+            num1 = 0;
+            num2 = 0;
+            gotNum1 = false;
+            result = 0;
+        }
 
-const display = document.querySelector('#display');
-
-
-function test (button) {
-    let pressed = button.id;
-    console.log(pressed);
-}
-
-const numbers = document.querySelectorAll(".numbers");
-
-numbers.forEach((numberButton) => {
-    numberButton.addEventListener('click', (e) => {
-        display.textContent += e.target.textContent;
-    })
-})
-
-const operators = document.querySelectorAll(".operators");
-
-operators.forEach((operatorButton) => {
-    operatorButton.addEventListener('click', (e) => {
-        display.textContent += e.target.textContent;
-    })
-})
-
-const clear = document.querySelector(".clear");
-
-clear.addEventListener('click', () => {
-    display.textContent = "";
-});
-
-const equals = document.querySelector(".equals");
-
-equals.addEventListener('click', () => {
-    display.textContent = "";
-});
+    });
+} // End for
